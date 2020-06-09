9b7cf9d6808efdff2853c6ee6afdd376290a56e2
BradenFreeman <66689577+BradenFreeman@users.noreply.github.com>
2020-06-09 13:14:34 -0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("How you doin?");
+            Console.WriteLine("Avada Kadavra!");
+            Console.WriteLine("Hi Randy!!!!!!");
+
+            MyText=
+        }
+    }
+}
