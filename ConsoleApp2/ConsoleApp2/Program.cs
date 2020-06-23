4bd78afa757daf3a949a0ad3627583c16f6bfca0
Seth Quincy <1994Braden@gmail.com>
2020-06-23 10:49:47 -0500

    added

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab5_2
+{
+    class Program
+    {
+
+        public enum Days
+        {
+            Sunsday,
+            Moonsday,
+            Twosday,
+            WedNesDay,
+            Thorsday,
+            Frysday,
+            Saturnsday
+        }
+
+        static void Main(string[] args)
+        {
+            //List to hold the Tasks
+            List<string> taskList = new List<string>();
+
+            foreach (Days day in Enum.GetValues(typeof(Days)))
+            {
+                Console.WriteLine("Enter your task for " + day);
+                string task = day + " : " + Console.ReadLine();
+                taskList.Add(task);
+            }
+
+            foreach (string task in taskList)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
