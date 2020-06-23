c2dabb85d3dd957e5833c6de819598841ce42886
Seth Quincy <1994Braden@gmail.com>
2020-06-23 11:22:11 -0500

    added flight

--- Lab5_4/Lab5_4/Program.cs
+++ Lab5_4/Lab5_4/Program.cs
@@ -18,6 +18,7 @@ namespace _5_4LabStarter
             public bool canShootFireBalls;
             public bool isJumping;
             public int numOfCoins;
+            public bool isFlying;
 
         }
 
@@ -29,31 +30,33 @@ namespace _5_4LabStarter
             mario.sizeOfMario = Mario.Size.small;
             mario.canShootFireBalls = false;
             mario.isJumping = false;
+            mario.isFlying = false;
             mario.numOfCoins = 100;
 
 
             Console.WriteLine("Mario is running through the level and meets a Gooba. Set Mario to jumping so he can kill it");
             mario.isJumping = true;
 
+            Console.WriteLine("Mario jumps off the Goomba and gets hit by a Bullet Bill. Lose one life.");
+            mario.numOfLives -= 3;
+
             Console.WriteLine("Mario squashes the Gooba. Now he needs to land!");
             mario.isJumping = false;
-
 
             Console.WriteLine("Mario hits a block and finds a 1UP. Increase Mario's life by one.");
             mario.numOfLives += 3;
 
-
             Console.WriteLine("Mario finds a secret Fire Flower and can now shoot fireballs. Change mario!");
             mario.canShootFireBalls = true;
-
 
             Console.WriteLine("Mario finds another power up mushroom and gets big. Change Mario");
             mario.sizeOfMario = Mario.Size.large;
 
-
             Console.WriteLine("Mario finds a stash of gold coins. Give Mario 10 coins.");
             mario.numOfCoins += 10;
 
+            Console.WriteLine("Mario finds a foxtail and is able to fly. Give Mario Flight.");
+            mario.isFlying = true;
 
             Console.WriteLine("Mario finshes the level! Change Mario's current level to World 1-2");
             mario.currentLevel = "World 1-2";
