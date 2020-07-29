dea7f8a33a544ceab1b107a175cecbcbb633bf8c
Seth Quincy <1994Braden@gmail.com>
2020-07-28 19:29:25 -0500

    Labs

---
+++ Lab2_4/Lab2_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+
+namespace Lab2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int First = 19;
+            int Second = 244;
+            Console.WriteLine(First * Second);
+            string myText = "Some text";
+            Console.WriteLine(myText);
+            string moreText = " goes here.";
+            Console.WriteLine(moreText);
+            Console.WriteLine(myText + moreText);
+            string color1 = "Yellow";
+            string color2 = "Red";
+            bool isYellow = true;
+            bool isGreen = false;
+            Console.WriteLine(color1, isYellow, color2, isGreen);
+            double numbers;
+            numbers = (Second / First);
+            Console.WriteLine(numbers);
+            Console.WriteLine("Break Point Here.");
+            string textToNumber = "80473";
+            int results = 99 * int.Parse(textToNumber);
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine(rightNow);
+        }
+    }
+}
